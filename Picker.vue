92bbbcf3e1b288f3436da85d625282826d7bf343
axlbun <axlbun@gmail.com>
2018-04-03 20:26:40 +0300

    Added README

--- Picker.vue
+++ Picker.vue
@@ -50,220 +50,205 @@
 </template>
 
 <script>
-    import data from './data';
+import data from "./data";
 
-    import Category from './Category.vue';
-    import RecentEmojis from './RecentEmojis';
-    import EmojiIndex from './emoji-index';
+import Category from "./Category.vue";
+import RecentEmojis from "./RecentEmojis";
+import EmojiIndex from "./emoji-index";
 
-    let start = null;
+let start = null;
 
-    export default {
-        beforeMount ()
-        {
-            if(window.performance) {
-              start = performance.now();
-            }
-        },
-        mounted()
-        {
-            this.recent = RecentEmojis.load();
-
-            setTimeout(() => {
-                this._updateHeights();
-            }, 1);
-
-            if(window.performance) {
-                console.info('emojipicker ready in ', performance.now() - start);
-            };
-
-            EventBus.listen('global::on-escape-pressed', () => {
-                EventBus.fire('hide-emoji-picker');
-            });
-
-            this.search = null;
-            this._updateHeights();
-            setTimeout(() => {
-                this.$refs.search.focus();
-            }, 1);
-        },
-        data()
-        {
-            return {
-                search: null,
-
-                activeCategory: "Recent",
-                availableCategory: true,
-                categoriesTopPosition: {},
-
-                scrollTop: 0,
-                categoryBounds: {},
-
-                recent: {},
-            }
-        },
-        watch: {
-            search(value) {
-                this.availableCategory = !value;
-            }
-        },
-        props: {
-            emojiesInRow: {
-                type: Number,
-                default: 8
-            },
-
-            emojiesViewHeight: {
-                type: Number,
-                default: 265
-            }
-        },
-        computed: {
-            pickerListStyle () {
-                if(this.search) return {};
-
-                let height = 0;
-
-                for(let category of this.getCategories()) {
-                    if(!this.categoryBounds[category]) {
-                        Vue.set(this.categoryBounds, category, {
-                            top: height,
-                            bottom: 0
-                        })
-                    }
-                    else {
-                        this.categoryBounds[category].top = height;
-                    }
-
-                    let categoryHeight = 0;
-
-                    categoryHeight += 27;
-                    categoryHeight += 30 * Math.ceil(this.category(category).length / this.emojiesInRow);
-
-                    height += categoryHeight;
-
-                    this.categoryBounds[category].bottom = categoryHeight;
-                }
-
-                return {
-                    'height': height + 'px'
-                }
-            },
-            searchEmojis()
-            {
-                let emojis = EmojiIndex.search(this.search, 48);
-                return emojis.map((item) => {return item.id;})
-            }
-        },
-        methods: {
-            bounds(category) {
-                return this.categoryBounds[category] || false;
-            },
-            handleOutside () {
-                this.$emit('click-outside');
-            },
-            setCategoryTopPosition(category, top)
-            {
-                this.categoriesTopPosition[category] = top;
-            },
-            specialEmojies(category)
-            {
-                if(category == 'Recent') {
-                    return RecentEmojis.create(this.recent)
-                            .getKeys();
-                }
-            },
-            getCategories()
-            {
-                return [
-                    "Recent",
-                    "People",
-                    "Nature",
-                    "Foods",
-                    "Activity",
-                    "Places",
-                    "Objects",
-                    "Symbols",
-                    "Flags"
-                ];
-            },
-            scroll(e)
-            {
-                this.scrollTop = e.target.scrollTop;
-                this.updateActiveCategory(e.target.scrollTop);
-            },
-            updateActiveCategory(top)
-            {
-                let keys = Object.keys(this.categoriesTopPosition).reverse();
-
-                for(let category of keys) {
-                    if(top >= this.categoryBounds[category].top) {
-                        this.activeCategory = category;
-                        return;
-                    }
-                }
-            },
-            getEmoji(emoji)
-            {
-                return data.emojis[emoji];
-            },
-
-            categoryNavImageSrc(category)
-            {
-                return './img/emoji/' + (category.toLowerCase()) + '.svg';
-            },
-
-            goToCategory(category)
-            {
-                this.search = null;
-                setTimeout(() => {
-                    this.$refs.list.scrollTop = this.categoryBounds[category].top;
-                }, 1);
-            },
-
-            category(name)
-            {
-                let cat = [];
-
-                if(name == 'Recent') {
-                    return Object.keys(this.recent);
-                }
-                else {
-                    let foundCategory = data.categories.filter((category) => {
-                        return category.name == name;
-                    });
-
-                    cat = (!foundCategory[0]) ? [] : foundCategory[0].emojis;
-                }
-
-                return cat;
-            },
-
-            emojiClicked(item)
-            {
-                RecentEmojis.create(this.recent).add(item);
-                this.$emit('emoji-clicked', item);
-            },
-            selectFirstSearchEmoji()
-            {
-                if(!this.search || !this.searchEmojis.length) return;
-
-                this.emojiClicked(this.getEmoji(this.searchEmojis[0]));
-            },
-            emptySearch()
-            {
-                this.search = null;
-            },
-
-            _updateHeights()
-            {
-                for(let category of this.getCategories()) {
-                    this.categoriesTopPosition[category] = document.getElementById('category-' + category).offsetTop;
-                }
-            }
-        },
-        components: {
-            'category': Category
-        }
+export default {
+  beforeMount() {
+    if (window.performance) {
+      start = performance.now();
     }
+  },
+  mounted() {
+    this.recent = RecentEmojis.load();
+
+    setTimeout(() => {
+      this._updateHeights();
+    }, 1);
+
+    if (window.performance) {
+      console.info("emojipicker ready in ", performance.now() - start);
+    }
+
+    EventBus.listen("global::on-escape-pressed", () => {
+      EventBus.fire("hide-emoji-picker");
+    });
+
+    this.search = null;
+    this._updateHeights();
+    setTimeout(() => {
+      this.$refs.search.focus();
+    }, 1);
+  },
+  data() {
+    return {
+      search: null,
+
+      activeCategory: "Recent",
+      availableCategory: true,
+      categoriesTopPosition: {},
+
+      scrollTop: 0,
+      categoryBounds: {},
+
+      recent: {}
+    };
+  },
+  watch: {
+    search(value) {
+      this.availableCategory = !value;
+    }
+  },
+  props: {
+    emojiesInRow: {
+      type: Number,
+      default: 8
+    },
+
+    emojiesViewHeight: {
+      type: Number,
+      default: 265
+    }
+  },
+  computed: {
+    pickerListStyle() {
+      if (this.search) return {};
+
+      let height = 0;
+
+      for (let category of this.getCategories()) {
+        if (!this.categoryBounds[category]) {
+          Vue.set(this.categoryBounds, category, {
+            top: height,
+            bottom: 0
+          });
+        } else {
+          this.categoryBounds[category].top = height;
+        }
+
+        let categoryHeight = 0;
+
+        categoryHeight += 27;
+        categoryHeight +=
+          30 * Math.ceil(this.category(category).length / this.emojiesInRow);
+
+        height += categoryHeight;
+
+        this.categoryBounds[category].bottom = categoryHeight;
+      }
+
+      return {
+        height: height + "px"
+      };
+    },
+    searchEmojis() {
+      let emojis = EmojiIndex.search(this.search, 48);
+      return emojis.map(item => {
+        return item.id;
+      });
+    }
+  },
+  methods: {
+    bounds(category) {
+      return this.categoryBounds[category] || false;
+    },
+    handleOutside() {
+      this.$emit("click-outside");
+    },
+    setCategoryTopPosition(category, top) {
+      this.categoriesTopPosition[category] = top;
+    },
+    specialEmojies(category) {
+      if (category == "Recent") {
+        return RecentEmojis.create(this.recent).getKeys();
+      }
+    },
+    getCategories() {
+      return [
+        "Recent",
+        "People",
+        "Nature",
+        "Foods",
+        "Activity",
+        "Places",
+        "Objects",
+        "Symbols",
+        "Flags"
+      ];
+    },
+    scroll(e) {
+      this.scrollTop = e.target.scrollTop;
+      this.updateActiveCategory(e.target.scrollTop);
+    },
+    updateActiveCategory(top) {
+      let keys = Object.keys(this.categoriesTopPosition).reverse();
+
+      for (let category of keys) {
+        if (top >= this.categoryBounds[category].top) {
+          this.activeCategory = category;
+          return;
+        }
+      }
+    },
+    getEmoji(emoji) {
+      return data.emojis[emoji];
+    },
+
+    categoryNavImageSrc(category) {
+      return "./img/emoji/" + category.toLowerCase() + ".svg";
+    },
+
+    goToCategory(category) {
+      this.search = null;
+      setTimeout(() => {
+        this.$refs.list.scrollTop = this.categoryBounds[category].top;
+      }, 1);
+    },
+
+    category(name) {
+      let cat = [];
+
+      if (name == "Recent") {
+        return Object.keys(this.recent);
+      } else {
+        let foundCategory = data.categories.filter(category => {
+          return category.name == name;
+        });
+
+        cat = !foundCategory[0] ? [] : foundCategory[0].emojis;
+      }
+
+      return cat;
+    },
+
+    emojiClicked(item) {
+      RecentEmojis.create(this.recent).add(item);
+      this.$emit("emoji-clicked", item);
+    },
+    selectFirstSearchEmoji() {
+      if (!this.search || !this.searchEmojis.length) return;
+
+      this.emojiClicked(this.getEmoji(this.searchEmojis[0]));
+    },
+    emptySearch() {
+      this.search = null;
+    },
+
+    _updateHeights() {
+      for (let category of this.getCategories()) {
+        this.categoriesTopPosition[category] = document.getElementById(
+          "category-" + category
+        ).offsetTop;
+      }
+    }
+  },
+  components: {
+    category: Category
+  }
+};
 </script>
